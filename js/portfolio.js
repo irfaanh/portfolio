b1469124b9a1e069022b48cabdcebac65b088398
irfan <mac@MacBook-Air.local>
2024-06-02 16:18:12 +0530

    animation edited

--- js/portfolio.js
+++ js/portfolio.js
@@ -31,25 +31,25 @@ const sr = ScrollReveal({
     reset: true
 })
 
-sr.reveal('.home__title',{})
-sr.reveal('.button',{delay: 200})
-sr.reveal('.home__img',{delay: 400})
-sr.reveal('.home__social-icon',{interval: 200})
+// sr.reveal('.home__title',{})
+// sr.reveal('.button',{delay: 200})
+// sr.reveal('.home__img',{delay: 400})
+// sr.reveal('.home__social-icon',{interval: 200})
 
 
 // sr.reveal('.about__img',{})
 // sr.reveal('.about__subtitle',{delay: 200})
 // sr.reveal('.about__text',{delay: 400})
 
-sr.reveal('.skills__subtitle',{})
-sr.reveal('.skills__text',{delay: 200})
-sr.reveal('.skills__data',{interval: 400})
+// sr.reveal('.skills__subtitle',{})
+// sr.reveal('.skills__text',{delay: 200})
+// sr.reveal('.skills__data',{interval: 400})
 
 
-sr.reveal('.work__img',{interval: 400})
+// sr.reveal('.work__img',{interval: 400})
 
 
-sr.reveal('.contact__input',{interval: 400})
+// sr.reveal('.contact__input',{interval: 400})
 
 
 // function sendMail(){
